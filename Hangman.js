b50ffbb6039924e5e506a1acf3a25ddbed6514a2
Jim Holmberg <hasenpfad1@gmail.com>
2019-03-10 16:24:43 -0500

    Removed console logs for easier code reading

--- Hangman.js
+++ Hangman.js
@@ -21,7 +21,6 @@ function wordLoad()
 			{
 			hangWord = hangWord.toLowerCase();
 			hangWordArray = hangWord.split("");
-			console.log("Initial value fo hangword array is " + hangWordArray);
 			$("hangWord").value = "";
 			}
 	
@@ -30,18 +29,13 @@ function wordLoad()
 	for (var i=0; i < hangWord.length; i++) 
 		{
 		wordGuessArray[i] = "__ ";
-		}
-		console.log(" WordGuessArray is is an array is " + Array.isArray(wordGuessArray));
-		console.log("The initlal wordGuess value is " +wordGuessArray);
-		
+		}		
 	$("trueLetters").firstChild.nodeValue = wordGuessArray.join("");
-	
 	}
 	
 	function lGuess() 
 	{
 	lGuess = $("playerGuess").value;
-	console.log("lGuess is " + lGuess);
 	
 	if (lGuess.search(/[^a-z]/i) !==-1 || lGuess.length > 1)
 		{
@@ -52,39 +46,20 @@ function wordLoad()
 		{
 		lGuess = lGuess.toLowerCase();
 		}
-	console.log("lGuess after .toLowerCase is " + lGuess);
-	
-	
-	/*for(var h = 0; h < wordGuess.length; h++){
-		wordGuessArray[h]= wordGuess[h];
-	}
-	
-	wordGuessArray = wordGuess.split(" ");
-	console.log("wordGuessArray is " + wordGuessArray);
-	*/
 	var indices = [];
-	console.log("Hangword in lGuess function is " + hangWord);
 	for (var k = 0; k < hangWordArray.length; k++)
 		{
-			console.log("hangWordArray "+ [k] + " is " + hangWordArray[k]);
 		if (hangWordArray[k] === lGuess) 
 			{
-			indices.push(k);
-			
+			indices.push(k);			
 			}
 		}
 	for (var l=0; l<indices.length; l++)
 		{
-			var m = indices[l];
-			console.log("M is " + m);
+		var m = indices[l];
 		wordGuessArray[m]= lGuess;
-		
-		console.log(wordGuessArray[m]);
 		$("trueLetters").firstChild.nodeValue = wordGuessArray.join(" ");
-		console.log("The guessword string is " + $("trueLetters").firstChild.nodeValue);
-		
 		}
-	console.log(" ");
  	}
 
 window.onload =  function()
